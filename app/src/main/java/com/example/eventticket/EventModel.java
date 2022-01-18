6c6b2839e5dec947ac0482d219ca2cc317c71f68
Kaua Duarte <kfrutuoso49@gmail.com>
2022-01-18 10:54:21 -0300

    Criação do splash e definição de cores

---
+++ app/src/main/java/com/example/eventticket/EventModel.java
@@ -0,0 +1,37 @@
+package com.example.eventticket;
+
+public class EventModel {
+    String name, local, hour, date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
