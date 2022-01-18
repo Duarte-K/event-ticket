f0dd1d1100eec3272427eed2c4f7c7b18275701a
Kaua Duarte <kfrutuoso49@gmail.com>
2022-01-18 17:09:18 -0300

    checagem de campos de login e cadastro

--- app/src/main/java/com/example/eventticket/activities/MainActivity.java
+++ app/src/main/java/com/example/eventticket/activities/MainActivity.java
@@ -31,8 +31,14 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
             }
         });
+    }
+
+    public boolean check(){
+
+        return true;
     }
 }
--- app/src/main/java/com/example/eventticket/activities/RegisterUserActivity.java
+++ app/src/main/java/com/example/eventticket/activities/RegisterUserActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,7 @@ import com.example.eventticket.R;
 
 public class RegisterUserActivity extends AppCompatActivity {
 
+    private EditText user, email, password;
     private Button btnRegisterUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,9 @@ public class RegisterUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_user);
 
         btnRegisterUser = findViewById(R.id.btn_registerUser);
+        user = findViewById(R.id.et_userRegister);
+        email = findViewById(R.id.et_emailRegister);
+        password = findViewById(R.id.et_passwordRegister);
 
         btnRegisterUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +32,23 @@ public class RegisterUserActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public boolean check(){
+        String us = user.getText().toString();
+        String em = email.getText().toString();
+        String pass = password.getText().toString();
+
+        if(us.equals("")){
+            user.setError("Preencha o campo para continuar");
+            return false;
+        }else if(em.equals("")){
+            email.setError("Preencha o campo para continuar");
+            return false;
+        }else if(pass.equals("")){
+            password.setError("Preencha o campo para continuar");
+            return false;
+        }
+        return true;
     }
 }
